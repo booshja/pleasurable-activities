b82cd0cf870c358f0bd621e057df72d62b7e59d2
Jacob Andes <jacob.andes@gmail.com>
2022-11-27 10:47:43 -0800

    chore: setup app structure

---
+++ shared/components/Providers.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+// dependencies
+// import {} from '';
+
+// @ts-ignore
+const Providers = ({ children }): JSX.Element => {
+  // TODO: add providers here to wrap around everything
+  return <>{children}</>;
+};
+
+export default Providers;
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      sm: "412px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      xxl: "1440px",
+    },
+    extend: {
+      colors: {},
+    },
+  },
+  plugins: [],
+};
